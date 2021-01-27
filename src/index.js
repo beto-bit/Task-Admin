a3238714362779e93f170759950571cf34ca2ed8
beto-bit <76757367+beto-bit@users.noreply.github.com>
2021-01-26 18:39:06 -0600

    🎱 Navegación I
    Básicamente los primeros arreglos en la navegación y un detalle en el Hot Reload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
 
 // Hot reload
 if (process.env.NODE_ENV !== 'production') {
   require('electron-reload')(__dirname, {
-    // Cosas
+    electron: path.join(__dirname, '../node-modules', '.bin', 'electron')
   });
 }
 
@@ -15,4 +16,26 @@ let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/views/index.html`);
+
+  // Navegación
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Navegación
+const templateMenu = [
+  // File
+  {
+    label: 'Archivo',
+    submenu: [
+      // Nueva Tarea
+      {
+        label: 'Nueva Tarea',
+        accelerator: 'Ctrl+N',
+        click() {
+          alert('New Product');
+        }
+      }
+    ]
+  }
+];
